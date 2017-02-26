ecad82d8807082223325e776462038f90bee8f07
Florian Kempenich <shockn745@gmail.com>
2017-02-26 12:51:29 +0000

    Display result to User

--- web/controllers/page_controller.ex
+++ web/controllers/page_controller.ex
@@ -1,11 +1,14 @@
 defmodule NelsonApproved.PageController do
   use NelsonApproved.Web, :controller
 
-  plug :show_why_by_default
-  def show_why_by_default(conn, _params), do: assign(conn, :show_why?, true)
+  plug :put_default_values
+  def put_default_values(conn, _params) do
+    conn
+    |> assign(:show_why?, true)
+    |> assign(:result, :no_result)
+  end
 
   def index(conn, _params) do
-    show_why_by_default(conn, [])
     render conn, "index.html"
   end
 
@@ -15,6 +18,6 @@ defmodule NelsonApproved.PageController do
 
   def check(conn, %{"check" => %{"food" => food}}) do
     IO.inspect food
-    render conn, "index.html"
+    render conn, "index.html", result: :approved
   end
 end
--- web/views/page_view.ex
+++ web/views/page_view.ex
@@ -1,4 +1,27 @@
 defmodule NelsonApproved.PageView do
   use NelsonApproved.Web, :view
 
+  @approved "Great, it's approved :D :D"
+  @not_approved "Stay off that food"
+  @unknown "Actually not sure about that one"
+
+  def show_result(:approved) do
+    make_alert "alert-success", @approved
+  end
+  def show_result(:not_approved) do
+    make_alert "alert-danger", @not_approved
+  end
+  def show_result(:unknown) do
+    make_alert "alert-warning", @unknown
+  end
+  def show_result(_), do: ""
+
+
+  defp make_alert(class, content) do
+    class = "alert " <> class
+    content_tag :h1, class: class, role: "alert" do
+      content
+    end
+  end
+
 end
