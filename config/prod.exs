e64fc4fc4531383205898fdd6ee0bc9245c2d2c1
Florian Kempenich <shockn745@gmail.com>
2017-02-26 14:36:29 +0000

    Update heroku url config

--- config/prod.exs
+++ config/prod.exs
@@ -13,8 +13,9 @@ use Mix.Config
 # which you typically run after static files are built.
 config :nelson_approved, NelsonApproved.Endpoint,
   http: [port: {:system, "PORT"}],
-  url: [scheme: "https", host: "ancient-stream-67940.herokuapp.com", port: 443],
-  force_ssl: [rewrite_on: [:x_forwarded_proto]],
+  # url: [scheme: "https", host: "ancient-stream-67940.herokuapp.com", port: 443],
+  # force_ssl: [rewrite_on: [:x_forwarded_proto]],
+  url: [host: "nelsonapproved.tk"],
   cache_static_manifest: "priv/static/manifest.json",
   secret_key_base: System.get_env("SECRET_KEY_BASE")
 
