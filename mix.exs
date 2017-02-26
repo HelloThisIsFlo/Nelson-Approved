6669b2a6d0e129b1b98df672468cce9dca70bcb4
Florian Kempenich <shockn745@gmail.com>
2017-02-26 16:21:48 +0000

    Move Application to different module

--- mix.exs
+++ mix.exs
@@ -17,7 +17,7 @@ defmodule NelsonApproved.Mixfile do
   #
   # Type `mix help compile.app` for more information.
   def application do
-    [mod: {NelsonApproved, []},
+    [mod: {NelsonApproved.Application, []},
      applications: [:phoenix, :phoenix_pubsub, :phoenix_html, :cowboy, :logger, :gettext,
                     :phoenix_ecto, :postgrex]]
   end
