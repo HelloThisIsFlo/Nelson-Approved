79bce4d1a2e5c8725dbc417215f67f87e00d958f
Florian Kempenich <shockn745@gmail.com>
2017-03-05 08:14:07 +0000

    Add HTTPotion & Poison deps

--- mix.exs
+++ mix.exs
@@ -19,7 +19,7 @@ defmodule NelsonApproved.Mixfile do
   def application do
     [mod: {NelsonApproved.Application, []},
      applications: [:phoenix, :phoenix_pubsub, :phoenix_html, :cowboy, :logger, :gettext,
-                    :phoenix_ecto, :postgrex, :comeonin]]
+                    :phoenix_ecto, :postgrex, :comeonin, :httpotion]]
   end
 
   # Specifies which paths to compile per environment.
@@ -38,7 +38,10 @@ defmodule NelsonApproved.Mixfile do
      {:phoenix_live_reload, "~> 1.0", only: :dev},
      {:gettext, "~> 0.11"},
      {:cowboy, "~> 1.0"},
-     {:comeonin, "~> 3.0"}]
+     {:comeonin, "~> 3.0"},
+     {:poison, "~> 2.2.0"},
+     {:httpotion, "~> 3.0.2"}
+    ]
   end
 
   # Aliases are shortcuts or tasks specific to the current project.
