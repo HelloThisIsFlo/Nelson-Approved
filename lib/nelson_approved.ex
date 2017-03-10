1654dc41749e08f6733992870756ca53f2e5cf0f
Florian Kempenich <shockn745@gmail.com>
2017-03-10 07:27:40 +0000

    Refactor NelsonApproved

--- lib/nelson_approved.ex
+++ lib/nelson_approved.ex
@@ -21,6 +21,13 @@ defmodule NelsonApproved do
 
   @behaviour NelsonApproved.Behaviour
 
+
+  @spec find_closest_match(String.t, [String.t]) :: String.t
+  def find_closest_match(food_name, all_food_names) do
+    all_food_names
+    |> Enum.max_by(&String.jaro_distance(&1, food_name))
+  end
+
   @spec approved?(String.t) :: Response.t
   def approved?(food) when is_bitstring(food) do
     food =
@@ -34,33 +41,38 @@ defmodule NelsonApproved do
   end
 
   defp do_approved(food, is_food_name?, database_value, ai_value)
-  defp do_approved(   _, false, _, _),                   do: %Response{approved?: :unknown, using_ai?: false}
-  defp do_approved(   _, true, :approved, _),            do: %Response{approved?: :approved, using_ai?: false}
-  defp do_approved(   _, true, :not_approved, _),        do: %Response{approved?: :not_approved, using_ai?: false}
-  defp do_approved(   _, true, :unknown, :approved),     do: %Response{approved?: :approved, using_ai?: true}
-  defp do_approved(   _, true, :unknown, :not_approved), do: %Response{approved?: :not_approved, using_ai?: true}
+  defp do_approved(   _, false, _, _),                     do: %Response{approved?: :unknown, using_ai?: false}
+  defp do_approved(   _, true, :approved, _),              do: %Response{approved?: :approved, using_ai?: false}
+  defp do_approved(   _, true, :not_approved, _),          do: %Response{approved?: :not_approved, using_ai?: false}
+  defp do_approved(   _, true, :not_found, :approved),     do: %Response{approved?: :approved, using_ai?: true}
+  defp do_approved(   _, true, :not_found, :not_approved), do: %Response{approved?: :not_approved, using_ai?: true}
+  defp do_approved(   _, true, :not_found, :unknown),      do: %Response{approved?: :unknown, using_ai?: true}
   defp do_approved(food, true, :not_fetched, _) do
     database_resp =
       food
       |> find_one_by_name
       |> is_approved?
+
+    log(food, database_resp, false)
     do_approved(food, true, database_resp, :not_fetched)
   end
-  defp do_approved(food, true, :unknown, :not_fetched) do
-    Logger.debug("Not Found in Database! | Food: " <> food)
-    case ArtificialIntelligence.is_processed_food?(food) do
-      true ->
-        Logger.debug("AI Disapproved! | Food: " <> food)
-        %Response{approved?: :not_approved, using_ai?: true}
-      false ->
-        Logger.debug("AI Approved! | Food: " <> food)
-        %Response{approved?: :approved, using_ai?: true}
-      _ ->
-        Logger.debug("AI Has no idea! | Food: " <> food)
-        %Response{approved?: :unknown, using_ai?: true}
-    end
+  defp do_approved(food, true, :not_found, :not_fetched) do
+    ai_value =
+      ArtificialIntelligence.is_processed_food?(food)
+      |> convert_ai_value()
+
+    log(food, ai_value, true)
+    do_approved(food, true, :not_found, ai_value)
   end
 
+  defp is_approved?(%Food{approved: true}),  do: :approved
+  defp is_approved?(%Food{approved: false}), do: :not_approved
+  defp is_approved?(_),                      do: :not_found
+
+  defp convert_ai_value(is_processed_food?)
+  defp convert_ai_value(true),  do: :not_approved
+  defp convert_ai_value(false), do: :approved
+  defp convert_ai_value(_),     do: :unknown
 
   @spec find_one_by_name(String.t) :: %Food{} | :not_found
   defp find_one_by_name(name) do
@@ -69,14 +81,11 @@ defmodule NelsonApproved do
     |> Repo.one
   end
 
-  defp is_approved?(%Food{approved: true}),  do: :approved
-  defp is_approved?(%Food{approved: false}), do: :not_approved
-  defp is_approved?(_),                      do: :unknown
-
-  @spec find_closest_match(String.t, [String.t]) :: String.t
-  def find_closest_match(food_name, all_food_names) do
-    all_food_names
-    |> Enum.max_by(&String.jaro_distance(&1, food_name))
+  defp log(food, :not_found, _) do
+    Logger.info("Food: " <> food <> " | Not found in Database ... checking with AI")
+  end
+  defp log(food, approved?, using_ai?) do
+    Logger.info("Food: " <> food <> " | Approved?: " <> to_string(approved?) <> " | Using_Ai?: " <> to_string(using_ai?))
   end
 
 end
